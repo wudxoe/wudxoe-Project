0f1d59b781dfa020b2ff6d5e5342f52346c932bd
wudxoe <120737105+wudxoe@users.noreply.github.com>
2024-06-24 01:34:32 +0900

    add Spring Boot
    add Spring Boot

---
+++ wudxoe-Project/src/main/java/com/wudxoe/wudxoe/Project/WudxoeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wudxoe.wudxoe.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WudxoeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WudxoeProjectApplication.class, args);
+	}
+
+}
---
+++ wudxoe-Project/src/test/java/com/wudxoe/wudxoe/Project/WudxoeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wudxoe.wudxoe.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WudxoeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
